ed570b22e3ef20ccd8bb8a5eae0cdf6c95201efa
Chrystian Calderon <chrystianralfc@gmail.com>
2026-01-14 20:39:14 -0400

    adding auth routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import UserModel from './models/user.model.js';
+import authRouter from './routes/auth.route.js';
 
 const app = express();
 
@@ -8,10 +11,10 @@ app.use(cors({
   origin: process.env.DOMAIN_FRONTEND,
   credentials: true,
 }));
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Distribuidora API');
-});
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use('/auth', authRouter({ UserModel }));
 
 export default app;
