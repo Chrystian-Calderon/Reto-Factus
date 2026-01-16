c964bc262c8cc8d61bdef9539b935931ceadd4c8
Chrystian Calderon <chrystianralfc@gmail.com>
2026-01-16 16:08:24 -0400

    adding producto routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,10 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import UserModel from './models/user.model.js';
 import BodegaModel from './models/bodega.model.js';
+import ProductoModel from './models/producto.model.js';
 import authRouter from './routes/auth.route.js';
 import bodegaRouter from './routes/bodega.route.js';
+import productoRouter from './routes/productos.route.js';
 
 const app = express();
 
@@ -19,5 +21,6 @@ app.use(cookieParser());
 
 app.use('/auth', authRouter({ UserModel }));
 app.use('/bodegas', bodegaRouter({ BodegaModel }));
+app.use('/productos', productoRouter({ ProductoModel }));
 
 export default app;
