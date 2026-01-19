d98a782f4abc221307638b369550bae1322d4003
Chrystian Calderon <chrystianralfc@gmail.com>
2026-01-18 20:16:33 -0400

    user verification method

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -17,6 +17,17 @@ class User {
     if (!user) return false;
     return comparePassword(password, user.password);
   }
+
+  async findByFields({ email, name, role }) {
+    const query = `
+      SELECT u.name, u.email, u.password, u.role_id, r.name AS role_name
+      FROM users u
+      LEFT JOIN roles r ON u.role_id = r.id
+      WHERE u.email = $1 AND u.name = $2 AND r.name = $3
+    `
+    const { rows } = await pool.query(query, [email, name, role]);
+    return rows[0];
+  }
 }
 
 export default new User();
